96026562f1cb504f9af16cceb521cfd75416db80
Horlicks Hung <horlicks.hung@chaogun.com>
2013-02-26 18:07:44 +0800

    add AllTest

---
+++ src/test/java/com/platinum/utils/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.platinum.utils.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestNumberUtils.class, TestSmallStringUtils.class })
+public class AllTests {
+
+}
